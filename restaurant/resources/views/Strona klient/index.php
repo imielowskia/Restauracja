62d8fe39aaffe8750c98b88429fce0aa146e46e3
Damian Świerk <84005683+DamianSwierk@users.noreply.github.com>
2021-05-20 18:16:30 +0200

    Revert "Revert "Strona główna / menu""
    This reverts commit 6286b5409921927a34c6fde77c5997e32d1671bd.

---
+++ restaurant/resources/views/Strona klient/index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+
+    <!-- Meta Tag do rensponsywnosci -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Style Index Css -->
+
+    <link rel="stylesheet" href="css/styleindex.css" >
+
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+    <title>Restaurant</title>
+
+
+
+
+
+
+</head>
+<body>
+
+<nav class="col-md-10 d-flex justify-content-center mb-3 mt-2 navbar navbar-light bg-light justify-content-between" style=" margin-left:auto; margin-right: auto; border-radius: 200px;">
+
+    <a class="navbar-brand"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-bag-check-fill" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zm-.646 5.354a.5.5 0 0 0-.708-.708L7.5 10.793 6.354 9.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
+        </svg></a>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="index">Strona Główna <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link active" href="menu">Menu <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link active" href="kontakt">Kontakt <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link active" href="galeria">Galeria <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link active" href="https://www.facebook.com/">Facebook <span class="sr-only">(current)</span></a>
+            </div>
+        </div>
+    </nav>
+</nav>
+
+<div class="container-fluid" >
+
+</div>
+
+
+
+
+
+
+<blockquote class="blockquote text-center text-light" style="margin-top: 15%; font-size: 50px;>
+    <p class="mb-3 ">Serdecznie zapraszymy do naszej nowej restauracji!</p>
+    <footer class="blockquote-footer text-light">Zjecie tutaj przepyszne <cite title="Source Title">dania</cite></footer>
+</blockquote>
+
+
+
+
+</body>
+</html>
---
+++ restaurant/resources/views/Strona klient/menu.php
@@ -0,0 +1,215 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+
+    <!-- Meta Tag do rensponsywnosci -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Style Index Css -->
+    <link rel="stylesheet" href="styleindex.css">
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+    <title>Restaurant</title>
+
+
+</head>
+<body style="background: red;">
+
+
+<nav class="col-md-10 d-flex justify-content-center mb-3 mt-2 navbar navbar-light bg-light justify-content-between" style=" margin-left:auto; margin-right: auto; border-radius: 200px;">
+
+    <a class="navbar-brand"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-bag-check-fill" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zm-.646 5.354a.5.5 0 0 0-.708-.708L7.5 10.793 6.354 9.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
+        </svg></a>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="index">Strona Główna <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link active" href="menu">Menu <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link active" href="kontakt">Kontakt <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link active" href="galeria">Galeria <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link active" href="https://www.facebook.com/">Facebook <span class="sr-only">(current)</span></a>
+            </div>
+        </div>
+    </nav>
+</nav>
+
+
+
+        <div class="container-fluid">
+            <!-- Menu Pizza -->
+            <div class="container">
+
+                <h1 style="text-align: center; color: white;">Pizza</h1>
+                <table class="table table-striped table-light">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nazwa</th>
+                        <th scope="col">Skład</th>
+                        <th scope="col">Cena 30cm / 40cm / 50cm</th>
+
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Capriciosa</td>
+                        <td>sos, ser, szynka, pieczarki</td>
+                        <td>15,00zł / 22,00zł / 32,00zł</td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Wegetariana</td>
+                        <td>sos, ser, pieczarki, papryka, kukurydza, cebula, oliwki zielone</td>
+                        <td>18,00zł / 23,50zł / 33,50zł</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Kebab Pizza</td>
+                        <td>sos, ser, kebab drobiowy, pieczarki, kukurydza</td>
+                        <td>18,00zł / 23,50zł / 33,50zł</td>
+                    </tr>
+                    </tbody>
+                </table>
+
+            </div>
+            <!-- Menu Pizza -->
+            <!-- Menu Dania -->
+            <div class="container">
+
+                <h1 style="text-align: center; color: white;">Dania</h1>
+                <table class="table table-striped table-light">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nazwa</th>
+                        <th scope="col">Skład</th>
+                        <th scope="col">połówka / całość</th>
+
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Kurczak w kokosie</td>
+                        <td>kurczak w panierce kokosowej, ryż, surówka</td>
+                        <td>11,00zł / 16,00zł</td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Kurczak na chrupiąco</td>
+                        <td>kurczak w chrupiącej panierce, ryż , surówka</td>
+                        <td>12,00zł / 16,50zł</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Kurczak 5 smaków</td>
+                        <td>kurczak w chrupiącej panierce, sos słodkokwaśny, sos serowy, sos jalapeno, ryż , surówka</td>
+                        <td>13,00zł / 18,00zł</td>
+                    </tr>
+                    </tbody>
+                </table>
+
+            </div>
+            <!-- Menu Dania -->
+            <!-- Menu Napoje -->
+            <div class="container" style="max-width: 500px;">
+
+                <h1 style="text-align: center; color: white;">Napoje</h1>
+                <table class="table table-striped table-light">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nazwa</th>
+                        <th scope="col">330ml / 500ml</th>
+
+
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Coca-Cola</td>
+                        <td>4zł / 6zł</td>
+
+
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Pepsi</td>
+                        <td>4zł / 6zł</td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Lipton</td>
+                        <td>4zł / 6zł</td>
+
+                    </tr>
+                    </tbody>
+                </table>
+                <!-- Menu Napoje -->
+                <!-- Menu Alkohol -->
+                <div class="container" style="max-width: 500px;">
+
+                    <h1 style="text-align: center; color: white;">Alkohol</h1>
+                    <table class="table table-striped table-light">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nazwa</th>
+                            <th scope="col">500ml / 1000ml</th>
+
+
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Żywiec</td>
+                            <td>8zł / 12zł</td>
+
+
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Lech</td>
+                            <td>9zł / 14zł</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Książęce</td>
+                            <td>10zł / 15zł</td>
+
+                        </tr>
+                        </tbody>
+                    </table>
+                    <!-- Menu Alkohol -->
+        </div>
+
+
+
+
+</body>
+</html>
+
