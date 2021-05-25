0dbe1c5a630eef9025c88e2ee7cf757755c3a8fb
Łukasz Sereda <s37237@s.pwste.edu.pl>
2021-05-25 22:34:39 +0200

    connect db

--- restaurant/app/Http/Controllers/KasaController.php
+++ restaurant/app/Http/Controllers/KasaController.php
@@ -2,26 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Zamowienie;
 use Illuminate\Http\Request;
 
 class KasaController extends Controller
 {
     public function index() {
-        //todo: pobierac te dane z bazy danych
-        $stoliki = [
-            ['numer' => 1, 'suma' => 55.49, 'zarchiwizowane' => false],
-            ['numer' => 2, 'suma' => 44.49, 'zarchiwizowane' => false],
-            ['numer' => 3, 'suma' => 33.49, 'zarchiwizowane' => false],
-            ['numer' => 4, 'suma' => 66.49, 'zarchiwizowane' => true],
-        ];
 
-        return view('kasa/index', ['stoliki' => $stoliki]);
+        $zamowienia = Zamowienie::all();
+
+        return view('kasa/index', ['zamowienia' => $zamowienia]);
     }
-
+/*
     public function zarchiwizuj(Request $request) {
         echo 'zarchiwizowano zamowienie o numerze ' . ($request->get("id"));
     }
-
+*/
     public function zaplac(Request $request) {
         echo 'Zaplacono za zamowienie o numerze ' . ($request->get("id"));
     }
