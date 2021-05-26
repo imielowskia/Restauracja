ba9761c78cf4256e0b5abf2deb1d57ba360fccef
cryptondr <83161554+cryptondr@users.noreply.github.com>
2021-05-26 19:17:43 +0200

    v1

--- restaurant/public/functions/WaiterName.php
+++ restaurant/public/functions/WaiterName.php
@@ -1,5 +1,7 @@
-<?php $name = DB::table('uzytkownicy')->where('id', '1')->pluck('imie');
-$surname = DB::table('uzytkownicy')->where('id', '1')->pluck('nazwisko');
+<?php
+$session_id = Session()->get('userID');
+$name = DB::table('uzytkownicy')->where('id', $session_id)->pluck('imie');
+$surname = DB::table('uzytkownicy')->where('id', $session_id)->pluck('nazwisko');
 ?>
 
 <p><?php
