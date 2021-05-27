8fc6d0b0204039e4f0136b024b1d41c8f0d61336
Szymon Smoliński <s37243@s.pwste.edu.pl>
2021-05-27 14:23:24 +0200

    Update header_kuchnia.blade.php

--- restaurant/resources/views/kuchnia/header_kuchnia.blade.php
+++ restaurant/resources/views/kuchnia/header_kuchnia.blade.php
@@ -9,7 +9,7 @@
     <title>KUCHNIA</title>
 
     <link href="{{asset('css/style_kuchnia.css')}}" rel="stylesheet">
-    <link href="{{asset('css/styles.css')}} rel="stylesheet">
+    <link href="{{asset('css/styles.css')}}" rel="stylesheet">
     <link rel="icon" href="https://cdn2.iconfinder.com/data/icons/fastfood-25/64/pizza-Italian-food-fast-food-512.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
