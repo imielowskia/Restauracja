c8095c410ac12b2626d4969c41d0bfc76cd354c5
Simon Hryszko <szevaa97@gmail.com>
2021-05-27 17:41:25 +0200

    Roles only from the list since now

--- restaurant/resources/views/admin/user/roles/new.blade.php
+++ restaurant/resources/views/admin/user/roles/new.blade.php
@@ -7,12 +7,19 @@
 <div class="container">
     <div class="row">
         <div class="col-4 offset-4">
-        <form action="new" method="POST">
-            @csrf
+            <form action="new" method="POST">
+                @csrf
+
+                <select class="btn-block custom-select" name="nazwa">
+                    <option value="Kierownik">Kierownik</option>
+                    <option value="Kelner">Kelner</option>
+                    <option value="Kasjer">Kasjer</option>
+                    <option value="Kucharz">Kucharz</option>
+                </select><br><br>
                 <button class="btn-block btn btn-primary">Dodaj!</button>
-        </form>
+            </form>
+        </div>
     </div>
-</div>
 </div>
 
 @endsection
